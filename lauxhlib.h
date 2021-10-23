b85425bf2ab7b929f000bba8a655d3ade814c4cf
Masatoshi Fukunaga <mah0x211@gmail.com>
2021-10-23 09:11:36 +0900

    Change lauxh_argerror macro to inline function

--- lauxhlib.h
+++ lauxhlib.h
@@ -339,12 +339,17 @@ static inline int lauxh_isinteger(lua_State *L, int idx)
 }
 
 /* check argument */
-#define lauxh_argerror(L, idx, fmt, ...)                                       \
- do {                                                                          \
-  char _errstr[255];                                                           \
-  snprintf(_errstr, 255, (fmt), ##__VA_ARGS__);                                \
-  luaL_argerror((L), (idx), _errstr);                                          \
- } while (0)
+static inline int lauxh_argerror(lua_State *L, int idx, const char *fmt, ...)
+{
+    char buf[255];
+    va_list ap;
+
+    va_start(ap, fmt);
+    vsnprintf(buf, 255, fmt, ap);
+    va_end(ap);
+
+    return luaL_argerror(L, idx, buf);
+}
 
 #define lauxh_argcheck(L, cond, idx, fmt, ...)                                 \
  do {                                                                          \
